9e75fcec74b0becc30ba0a635fe1bcf1200712d3
Gary Capell <gary.capell@gmail.com>
2012-12-08 10:11:37 +1100

    initial

---
+++ tile.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gcapell/dlx"
+	"log"
+)
+
+type board struct {
+	nSquares int
+	squares  []square
+	nTiles   int
+}
+
+type tile struct {
+	index   int
+	squares []square
+}
+
+type square struct {
+	x, y int
+}
+
+func main() {
+	tiles := asciiToTiles(tilesASCII)
+	b := parseBoard(boardASCII)
+	err, result := b.solve(tiles)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result)
+}
+
+func (b *board) solve(tiles []tile) (error, string) {
+	if problem := b.sanityCheck(tiles); problem != nil {
+		return problem, ""
+	}
+
+	b.nTiles = len(tiles)
+	d := dlx.New(b.nTiles + b.nSquares)
+	for _, t := range tiles {
+		for _, t2 := range permute(t) {
+			for _, p := range b.squares {
+				t3 := t2.translate(p)
+				if b.contains(t3) {
+					d.AddRow(b.dlxRow(t3))
+				}
+			}
+		}
+	}
+	result := d.Search()
+	if result == nil {
+		return errors.New("No solution found"), ""
+	}
+	return nil, b.text(result)
+}
+
+// Permute returns all the distinct permutations (rotate/flip/...) of a tile.
+func permute(t tile) []tile {
+	return nil // fixme
+}
+
+func (t tile) translate(offset square) tile {
+	return t // fixme
+}
+
+func (b *board) contains(t tile) bool {
+	return true // fixme
+}
+
+func (b *board) dlxRow(t tile) []int {
+	return nil // fixme
+}
+
+// Return string representation of solution
+func (b *board) text(dlxResult [][]int) string {
+	return "cannot represent solution" // fixme
+}
+
+func (b *board) sanityCheck(tiles []tile) error {
+	s := 0 // Total squares covered by all tiles
+	for _, t := range tiles {
+		s += len(t.squares)
+	}
+	if s != b.nSquares {
+		return fmt.Errorf("tiles cover %d squares, board has %d squares",
+			s, b.nSquares)
+	}
+	return nil
+}
+
+// Parse ascii drawing of board
+func parseBoard(s string) board {
+	return board{} // fixme
+}
+
+// Parse (newline-separated) ascii drawings of tiles
+func asciiToTiles(s string) []tile {
+	return nil // fixme
+}
---
+++ tiles.go
@@ -0,0 +1,52 @@
+package main
+
+const tilesASCII = `
+ xx
+xx
+ x
+ x
+
+ xxx
+xx
+ x
+
+  x
+xxxx
+x
+
+xxxx
+x
+x
+
+x
+xxx
+  xx
+
+ xx
+  x
+xxx
+
+xxxxx
+    x
+
+ xxx
+xx x
+
+xx
+ x
+ xxx
+
+xx
+ xxxx
+`
+
+const boardASCII = `
+ xxxxxx
+xxxxxxxx
+xxxxxxxx
+xxxxxxxx
+xxxxxxxx
+xxxxxxxx
+xxxxxxxx
+ xxxxxx
+`
