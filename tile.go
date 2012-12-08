c26fccad606b945a426de6827b71911178478b6d
Gary Capell <gary.capell@gmail.com>
2012-12-09 01:20:09 +1100

    Now really calls DLX... and really breaks

--- tile.go
+++ tile.go
@@ -10,7 +10,7 @@ import (
 
 type board struct {
 	tile
-	index map[square]int	// position of each square
+	index map[square]int // position of each square
 }
 
 type tile struct {
@@ -46,6 +46,7 @@ type square struct {
 func main() {
 	tiles := asciiToTiles(tilesASCII)
 	b, err := parseBoard(boardASCII)
+	log.Printf("board: %v", b)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -63,21 +64,29 @@ func (b *board) solve(tiles []tile) (error, string) {
 
 	nTiles := len(tiles)
 	d := dlx.New(nTiles + len(b.squares))
-	_, maxX, _, maxY := b.bounds()
+	_, _, maxX, maxY := b.bounds()
+	log.Printf("maxX:%d, maxY:%d", maxX, maxY)
 
+	rows := 0
 	for tileIndex, t := range tiles {
 		for _, t2 := range permute(t) {
 			_, tmaxX, _, tmaxY := t2.bounds()
 			for x := 0; x < maxX-tmaxX; x++ {
 				for y := 0; y < maxY-tmaxY; y++ {
 					t3 := t2.translate(x, y)
-					if b.contains(t3) {
-						d.AddRow(b.dlxRow(nTiles, tileIndex, t3))
+					if positions, ok := b.positions(t3); ok {
+						positions = append(positions, len(b.squares)+tileIndex)
+						log.Printf("Adding row %v", positions)
+						rows++
+						d.AddRow(positions)
+					} else {
+						log.Printf("couldn't place %v", t3)
 					}
 				}
 			}
 		}
 	}
+	log.Printf("%d rows", rows)
 	result := d.Search()
 	if result == nil {
 		return errors.New("No solution found"), ""
@@ -86,8 +95,84 @@ func (b *board) solve(tiles []tile) (error, string) {
 }
 
 // Permute returns all the distinct permutations (rotate/flip/...) of a tile.
-func permute(t tile) []tile {
-	return nil // fixme
+func permute(orig tile) []tile {
+	reply := []tile{orig}
+
+	f := func(t tile, fn func(square) square) tile {
+		s2 := make([]square, len(t.squares))
+		copy(s2, t.squares)
+		for i, s := range s2 {
+			s2[i] = fn(s)
+		}
+		normalise(s2)
+		t2 := tile{s2}
+		if !contains(reply, t2) {
+			reply = append(reply, t2)
+		}
+		return t2
+	}
+
+	r := f(orig, ror)
+	r = f(r, ror)
+	r = f(r, ror)
+
+	flipped := f(orig, flip)
+	r = f(flipped, ror)
+	r = f(r, ror)
+	r = f(r, ror)
+
+	log.Printf("permute %v -> %d, %v", orig, len(reply), reply)
+	return reply
+}
+
+func contains(bag []tile, t tile) bool {
+	for _, t2 := range bag {
+		if matches(t2, t) {
+			return true
+		}
+	}
+	return false
+}
+
+func matches(t, t2 tile) bool {
+	bag := make(map[square]bool)
+	for _, s := range t.squares {
+		bag[s] = true
+	}
+	for _, s := range t2.squares {
+		if !bag[s] {
+			return false
+		}
+	}
+	return true
+}
+
+func normalise(ss []square) {
+	minX, minY := ss[0].x, ss[0].y
+
+	for _, s := range ss {
+		if s.x < minX {
+			minX = s.x
+		}
+		if s.y < minY {
+			minY = s.y
+		}
+	}
+	if minX == 0 && minY == 0 {
+		return
+	}
+	for i, s := range ss {
+		ss[i] = square{s.x - minX, s.y - minY}
+	}
+}
+
+// Rotate tile quarter-turn clockwise
+func ror(s square) square {
+	return square{s.y, -s.x}
+}
+
+func flip(s square) square {
+	return square{-s.x, s.y}
 }
 
 func (t tile) translate(dx, dy int) tile {
@@ -100,17 +185,15 @@ func (t tile) translate(dx, dy int) tile {
 	return tile{s2}
 }
 
-func (b *board) contains(t tile) bool {
-	for _, s := range t.squares {
-		if _, ok := b.index[s]; !ok {
-			return false
+func (b *board) positions(t tile) ([]int, bool) {
+	reply := make([]int, len(t.squares))
+	var ok bool
+	for i, s := range t.squares {
+		if reply[i], ok = b.index[s]; !ok {
+			return reply, false
 		}
 	}
-	return true // fixme
-}
-
-func (b *board) dlxRow(nTiles, tileIndex int, t tile) []int {
-	return nil // fixme
+	return reply, true
 }
 
 // Return string representation of solution
